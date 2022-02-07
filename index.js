bd569fe3466f43f3ec6913956ca368804eee194b
affantaufiqur <affantaufiqur@gmail.com>
2022-02-07 11:46:23 +0700

    edit config

--- index.js
+++ index.js
@@ -114,6 +114,8 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('error', {err});
 });
 
-app.listen(3000, () => {
-    console.log('listen 3000')
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
+    console.log(`listen on port ${port}`)
 });
